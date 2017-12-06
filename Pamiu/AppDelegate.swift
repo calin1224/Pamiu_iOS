18972fc5e18e7abbc25a733d22aa4869612d97f2
Jeremy <calin1224@gmail.com>
2017-12-06 17:49:08 +0800

    test

--- Pamiu/AppDelegate.swift
+++ Pamiu/AppDelegate.swift
@@ -16,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        //指定root view controller
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let mainVC = MainViewController()
+        // 指定 root view controller
+        self.window?.rootViewController = mainVC
+        // 把 window 顯示出來
+        self.window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
